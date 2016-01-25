4f06f990fdfbd12009efda7b90f64a0693df9dbf
Karson Madden <krm@karsonm.local>
2016-01-25 10:36:50 -0800

    fixed gulp

--- source/app.js
+++ source/app.js
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Application = require('./components/Application.react');
 
-ReactDOM.render(<Application />, document.getElementById('react-application'));
+
+ReactDOM.render("hello world", document.getElementById('react-application'));
